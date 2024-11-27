c8e7132d9518e9be8930426e2b28dd8c1aeb82d2
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2024-11-27 17:32:53 +0700

    Entry Terdakwa

--- M8_5230411296_Hafizh Habiibi Lubis/M8_5230411296_Hafizh Habiibi Lubis.py
+++ M8_5230411296_Hafizh Habiibi Lubis/M8_5230411296_Hafizh Habiibi Lubis.py
@@ -19,6 +19,40 @@ class TilangApp:
         title_label.pack()
         title_app.pack()
 
+        # Frame
+        frame_left = tk.Frame(root, highlightbackground="black", highlightthickness=3)
+        frame_left.pack(side="left",padx=20, pady=20, ipadx=20, ipady=20, fill="both", expand=True)
+
+        frame_right = tk.Frame(root, highlightbackground="black", highlightthickness=3)
+        frame_right.pack(side="right", padx=20, pady=20, ipadx=20, ipady=20, fill="both", expand=True)
+
+        # Menu Terdakwa
+        td_menu = tk.Label(frame_left, text="DATA DIRI TERDAKWA", font=("Courier New", 15, "bold"))
+        td_menu.pack(anchor="center", pady=20)
+
+        # Nama Terdakwa
+        name_label = tk.Label(frame_left, text="NAMA TERDAKWA")
+        name_label.pack(anchor="w", padx=10)
+        self.name_entry = tk.Entry(frame_left)
+        self.name_entry.pack(fill="x", anchor="w", padx=10, pady=2)
+
+        # NIK Terdakwa
+        nik_label = tk.Label(frame_left, text="NIK TERDAKWA")
+        nik_label.pack(anchor="w", padx=10, pady=8)
+        self.nik_entry = tk.Entry(frame_left)
+        self.nik_entry.pack(fill="x", anchor="w", padx=10, pady=2)
+
+        # Alamat Terdakwa
+        alm_label = tk.Label(frame_left, text="ALAMAT TERDAKWA")
+        alm_label.pack(anchor="w", padx=10, pady=8)
+        self.alm_entry = tk.Entry(frame_left)
+        self.alm_entry.pack(fill="x", anchor="w", padx=10, pady=2)
+
+        # Menu Kendaraan
+        td_menu = tk.Label(frame_left, text="DATA KENDARAAN", font=("Courier New", 15, "bold"))
+        td_menu.pack(anchor="center", pady=20)
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = TilangApp(root)
