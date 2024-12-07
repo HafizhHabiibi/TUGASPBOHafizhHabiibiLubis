e198f5bc2ef4628aebd15430b7f29a243d0a8845
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2024-12-07 21:03:21 +0700

    membuat tabel pada database

--- M10_5230411296_Hafizh Habiibi Lubis/M10_5230411296_Hafizh Habiibi Lubis.py
+++ M10_5230411296_Hafizh Habiibi Lubis/M10_5230411296_Hafizh Habiibi Lubis.py
@@ -12,33 +12,33 @@ cur = conn.cursor()
 # membuat database
 # cur.execute("CREATE DATABASE penjualan")
 
-# membuat tabel produk
-cur.execute("""CREATE TABLE Produk (
-            Kode_Produk CHAR(10) NOT NULL PRIMARY KEY,
-            Nama_Produk VARCHAR(20),
-            Jenis_Produk VARCHAR(8)),
-            Harga INT(10))""")
+# # # membuat tabel pegawai
+# cur.execute("""CREATE TABLE Pegawai (
+#             NIK CHAR(16) NOT NULL PRIMARY KEY,
+#             Nama_Pegawai VARCHAR(25),
+#             Alamat_Pegawai VARCHAR(255))""")
 
-# membuat tabel pegawai
-cur.execute("""CREATE TABLE Pegawai (
-            NIK CHAR(16) NOT NULL PRIMARY KEY,
-            Nama_Pegawai VARCHAR(25),
-            Alamat_Pegawai VARCHAR(255)
-            )""")
+# # membuat tabel produk
+# cur.execute("""CREATE TABLE Produk (
+#             Kode_Produk CHAR(5) NOT NULL PRIMARY KEY,
+#             Nama_Produk VARCHAR(20),
+#             Jenis_Produk VARCHAR(8),
+#             Harga INT(10))""")
 
-# membuat tabel struk
+
+# # membuat tabel transaksi
+# cur.execute("""CREATE TABLE Transaksi (
+#             No_Transaksi CHAR(7) NOT NULL PRIMARY KEY,
+#             Detail_Transaksi VARCHAR(255))""")
+
+# membuat tabel struk membuat composite key 
 cur.execute("""CREATE TABLE Struk (
-            No_Transaksi CHAR(10) NOT NULL PRIMARY KEY,
+            NIK CHAR(16) NOT NULL,
+            Kode_Produk CHAR(5) NOT NULL,
+            No_Transaksi CHAR(7) NOT NULL,
             Jumlah_Produk INT(100),
-            Total_Harga VARCHAR(30))""")
-
-# membuat tabel transaksi
-cur.execute("""CREATE TABLE Transaksi (
-            Detail_Transaksi VARCHAR(255))""")
-
-# Tambah foreign key
-
-# # tabel struk
-# cur.execute("""ALTER TABLE Struk
-#             ADD FOREIGN KEY (NIK)
-#             REFERENCES Pegawai(NIK)""")
+            Total_Harga VARCHAR(30),
+            PRIMARY KEY (NIK, Kode_Produk, No_Transaksi),
+            FOREIGN KEY (NIK) REFERENCES Pegawai(NIK),
+            FOREIGN KEY (Kode_Produk) REFERENCES Produk(Kode_Produk),
+            FOREIGN KEY (No_Transaksi) REFERENCES Transaksi(No_Transaksi))""")
